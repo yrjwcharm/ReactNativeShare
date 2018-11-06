9fcc82b3af0fbf055917377cabb407468576f30f
yrjwcharm <xlyanrui@sina.com>
2018-11-06 13:05:00 +0800

    Initial commit

---
+++ app/src/main/java/com/reactnativeshare/MainApplication.java
@@ -0,0 +1,56 @@
+package com.reactnativeshare;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.reactnativeshare.module.SharePackage;
+import com.umeng.socialize.Config;
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.commonsdk.UMConfigure;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+              new SharePackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this,false);
+    // 此处配置类型，供后台分析各渠道时使用
+    Config.shareType = "react native";
+    // 初始化Umeng分享
+    UMConfigure.init(this,"友盟Key","umeng",UMConfigure.DEVICE_TYPE_PHONE,"");
+  }
+
+  // 配置平台key、secret信息
+  {
+    PlatformConfig.setWeixin("wx083bf496cbc48aec", "750e9075fa521c82274a9d548c399825");
+    PlatformConfig.setQQZone("1106207359", "3JjbG8aXMuh5w0sV");
+    PlatformConfig.setSinaWeibo("2733400964", "fac50980a44e3e3afd4bc968ea572887", "www.baidu.com");
+  }
+}
---
+++ app/src/main/wxapi/WXEntryActivity.java
@@ -0,0 +1,6 @@
+package com.reactnativeshare;
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity {
+
+}
